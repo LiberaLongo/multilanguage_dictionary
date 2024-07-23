e1995df550bd6fcd58683c552c8b60b7675cfe2e
LiberaLongo <liberalongo@studio.unibo.it>
2024-07-23 13:46:59 +0200

    insert.svelte

---
+++ src/components/Insert.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Language from './Language.svelte';
+	let languages = [
+		{ id: "English", myself: "English", pronunciation: false},
+		{ id: "Italian", myself: "Italiano", pronunciation: false},
+		{ id: "Chinese", myself: "汉语", pronunciation: true},
+		{ id: "Japanese", myself: "日本語", pronunciation: true},
+		{ id: "German", myself: "Deutsch", pronunciation: false},
+		{ id: "Russian", myself: "Русский", pronunciation: false},
+		{ id: "French", myself: "Français", pronunciation: false},
+	];
+</script>
+
+<h1>Insert</h1>
+<a href="/">home</a>
+<form action="/submit" method="POST">
+{#each languages as language}
+	<Language {...language} />
+{/each}
+<input type="submit" value="Insert" />
+</form>
+
+<style>
+</style>
---
+++ src/components/Language.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let id;
+	export let myself;
+	export let pronunciation=false;
+</script>
+
+<img src="images/{id}.png" alt={id}>
+<label for="{id}">{myself}</label>
+{#if pronunciation}
+<input type="text" name={id}>
+<input type="text" name="p_{id}">
+{:else}
+<input type="text" class="long" name={id}>
+{/if}
+<br>
+
+<style>
+	img { height: 10vh; width: 15vw; }
+	label { font-size: 3em; }
+	input[type=text] { font-size: 2em; width: 20vw; }
+	input[class=long] { width: 60vw; }
+</style>
+
